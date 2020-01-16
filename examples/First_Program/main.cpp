a6a26d49795f49dc1e05a9cf6071dd5f76ba0f39
Janis Joderi Shoferi <janis.joderishoferi@campus.tu-berlin.de>
2020-01-17 00:13:55 +0100

    Added examples directory and put current program as first example into it

---
+++ examples/First_Program/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <math.h>
+
+#include <core/scene/scene.h>
+#include <core/mesh/mesh.h>
+#include <core/camera/camera.h>
+#include <OpenGL/renderer/renderer.h>
+#include <OpenGL/material/OpenGLMaterial.h>
+#include <OpenGL/geometry/sphereGeometry/OpenGLSphereGeometry.h>
+#include <OpenGL/geometry/boxGeometry/OpenGLBoxGeometry.h>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#define WINDOW_WIDTH 640.0
+#define WINDOW_HEIGHT 480.0
+
+void setUp(CG::Scene &scene, CG::Camera &camera, CG::Renderer &renderer);
+void setUpInputHandling(GLFWwindow *window, CG::Scene &scene, CG::Camera &camera, CG::Renderer &renderer);
+
+float counter = 0.0f;
+
+int main(){
+  int success = glfwInit();
+
+  if(!success){
+    std::cerr << "Failed initializing GLFW\n";
+  }
+  
+  GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CG-Project", NULL, NULL);
+
+  glfwMakeContextCurrent(window);
+  //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+  glewInit();
+  fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+
+	fprintf(stdout, "OpenGL Version is: %s\n", glGetString(GL_VERSION));
+
+  CG::Scene scene{};
+  CG::Camera camera{ 1.0, 100, 45, WINDOW_WIDTH/WINDOW_HEIGHT };
+  CG::Renderer renderer{};
+
+
+  setUp(scene, camera, renderer);
+  setUpInputHandling(window, scene, camera, renderer);
+
+  while(!glfwWindowShouldClose(window)){
+	  renderer.render(scene, camera);
+	  glfwSwapBuffers(window);
+	  glfwPollEvents();
+    counter += 0.01;
+  }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+}
